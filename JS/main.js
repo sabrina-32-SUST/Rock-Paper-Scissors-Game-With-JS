be9e88c9c718054b3e521db67bae2e1685a3891d
Sabrina <sanutaayvi@gmail.com>
2020-12-21 14:58:25 +0600

    event listener

--- JS/main.js
+++ JS/main.js
@@ -11,5 +11,17 @@ const   paper_div = document.getElementById("p");
 const   scissors_div = document.getElementById("s");
 
 
+rock_div.addEventListener('click',  function(){
+    console.log("hey   you  click  on  Rock");
 
+})
+
+paper_div.addEventListener('click',  function(){
+    console.log("hey   you  click  on  Paper");
+
+})
+scissors_div.addEventListener('click',  function(){
+    console.log("hey   you  click  on  Scissor");
+
+})
 
