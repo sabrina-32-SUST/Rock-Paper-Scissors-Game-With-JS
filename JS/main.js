c86827d199d921674a39e9d439ebc965c3042b47
Sabrina <sanutaayvi@gmail.com>
2020-12-21 19:52:12 +0600

    almost  done

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,12 @@
 //var  declaration
-const   userScore = 0;
-const   compScore = 0;
+let   userScore = 0;
+let   compScore = 0;
 
 const    userScore_span = document.getElementById("user-score");
 const    compScore_span = document.getElementById("comp-score");
 const   scoreBoard_div = document.querySelector(".score-board");
 
-const   result_div = document.querySelector(".result");
+const   result_p = document.querySelector(".result > p");
 const   rock_div = document.getElementById("r");
 const   paper_div = document.getElementById("p");
 const   scissors_div = document.getElementById("s");
@@ -19,24 +19,51 @@ function  getCompChoice(){
      return  choices[rendomNumber];
 }
 
+function  converToward(letter){
+    if(letter ==="r") return "Rock";
+    if(letter ==="p") return "Paper";
+    return  "Scissor";
+
+
+}
+
 
 //function for   win
-function  win(){
-console.log("WIN");
+function  win(user ,  comp ){
+ userScore++;
+  userScore_span.innerHTML = userScore;
+  compScore_span.innerHTML = compScore;
+
+  const  smallUserWord = "user".fontsize(3).sup() ;
+  const  smallCompWord = "comp".fontsize(3).sup() ;
+
+   result_p.innerHTML = `${converToward(user)}${smallUserWord} beats  ${converToward(comp)}${smallCompWord}. You  win`;
+
+
 
 }
 //function for   lose
 
  
-function  lose(){
-    console.log("LOSE");
-
+function  lose(user ,  comp){
+     
+    compScore++;
+    userScore_span.innerHTML = userScore;
+    compScore_span.innerHTML = compScore;
+  
+    const  smallUserWord = "user".fontsize(3).sup() ;
+    const  smallCompWord = "comp".fontsize(3).sup() ;
+  
+     result_p.innerHTML = `${converToward(user)}${smallUserWord} loses to  ${converToward(comp)}${smallCompWord}. You  Lost...`;
 }
  
 //function for   draw
 
-function  draw(){
-    console.log("DRAW");
+function  draw(user ,  comp){
+
+    const  smallUserWord = "user".fontsize(3).sup() ;
+    const  smallCompWord = "comp".fontsize(3).sup() ;
+    result_p.innerHTML = `${converToward(user)}${smallUserWord} equals  ${converToward(comp)}${smallCompWord}. It's a   Draw`;
 
 }
  
@@ -48,17 +75,17 @@ function  game(userChoice){
          case "rs":
          case "pr":
          case "sp":
-             win();
+             win(userChoice,compChoice);
              break;
          case "rp":
          case "ps":
          case "sr":
-             lose();
+             lose(userChoice,compChoice);
               break;
         case "rr":
         case "pp":
         case "ss":
-            draw();
+            draw(userChoice,compChoice);
              break;
          
      }
